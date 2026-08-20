7da0070b8c64bc577d8b265b6b4c7a3fa1904c24
amandabomfimoliveira <amanda.oliveira@foursys.com.br>
2026-08-20 11:00:04 -0300

    first commit

---
+++ backend/src/main/java/br/com/fourbank/fourbank/FourbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.fourbank.fourbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourbankApplication.class, args);
+	}
+
+}
